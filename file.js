7932f704a3075e6a644ffe0e9872dcc870ad2c5d
payal <pv15052008@gmail.com>
2026-01-23 12:17:11 +0530

    using set interval time

--- file.js
+++ file.js
@@ -52,3 +52,16 @@ let b=12345;
 
 console.log(a);
 console.log(b);
+setInterval(() => {
+    const timeDisplay = document.getElementById('time-display');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
+}, 1000);
+let count = 1;
+const intervalId = setInterval(() => {
+  console.log(count);
+  count++;
+}, 1000);
